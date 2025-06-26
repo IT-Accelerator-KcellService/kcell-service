3857efb7157d73cbedba2068ff0a80a5bef11d1f
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-06-26 21:19:24 +0500

    feat: notification service

--- src/main/services/notificationService.js
+++ src/main/services/notificationService.js
@@ -1,5 +1,9 @@
-import {Notification} from "../models//init.js"
+import sgMail from '@sendgrid/mail';
+import {Notification, User} from "../models//init.js"
 import {ForbiddenError, NotFoundError} from "../errors/errors.js";
+import req from "express/lib/request.js";
+import {user} from "pg/lib/defaults.js";
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 class NotificationService {
     static async getNotificationsByUserId(userId) {
@@ -31,6 +35,71 @@ class NotificationService {
             throw new ForbiddenError("Forbidden")
         }
         return await Notification.destroy({where: {id}})
+    }
+
+    static async sendNotification({ userId, requestId = null, type, content = null }) {
+        if (!userId || !type) {
+            throw new Error("Missing required fields: userId or type");
+        }
+        const sender = await User.findByPk(userId);
+        switch (type) {
+            case 'new_request':
+                if (!sender || !sender.office_id) {
+                    throw new Error("Sender or office ID not found");
+                }
+
+                const admins = await User.findAll({
+                    where: {
+                        role: 'admin-worker',
+                        office_id: sender.office_id
+                    }
+                });
+
+                for (const admin of admins) {
+                    const notifTitle = "Новая заявка";
+                    const notifContent = `Пользователь из вашего офиса создал новую заявку(${requestId}).`;
+
+                    Notification.create({
+                        user_id: admin.id,
+                        request_id: requestId,
+                        title: notifTitle,
+                        content: notifContent,
+                        is_read: false
+                    });
+
+                    sgMail.send({
+                        to: admin.email,
+                        from: process.env.SENDGRID_EMAIL_FROM,
+                        subject: notifTitle,
+                        text: notifContent
+                    });
+                }
+
+                break;
+            case 'reject_request':
+                const notifTitle = "Заявка откланен";
+
+                await Notification.create({
+                    user_id: userId,
+                    request_id: requestId,
+                    title: notifTitle,
+                    content: content,
+                    is_read: false
+                });
+
+                await sgMail.send({
+                    to: sender.email,
+                    from: process.env.SENDGRID_EMAIL_FROM,
+                    subject: notifTitle,
+                    text: content
+                });
+                break;
+            case 'awaiting_assignment':
+
+                break;
+            default:
+                throw new Error(`Unknown notification type: ${type}`);
+        }
     }
 }
 
