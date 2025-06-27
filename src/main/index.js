866acf8c6a615f6ffa6443ee2b4dbb2743cfadda
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-06-28 02:04:12 +0500

    fix: origin

--- src/main/index.js
+++ src/main/index.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 3001
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://kcell-service.vercel.app'],
   credentials: true
 }));
 app.use(express.json())
