7fcae8f6ebcbe07b77a0d8880b3884dcbe789c64
damirablahat <damirablahat0@gmail.com>
2025-06-28 03:17:31 +0500

    fix[] fix logout

--- src/main/routes/authRoutes.js
+++ src/main/routes/authRoutes.js
@@ -5,12 +5,12 @@ const router = express.Router()
 
 router.post("/login", AuthService.login)
 router.post('/logout', (req, res) => {
-    res.clearCookie('token', {
+    return  res.clearCookie('token', {
         httpOnly: false,
         secure: false,
         sameSite: 'lax',
         path: '/',
-    }).redirect("http://localhost:5173");
+    });
 });
 
 export default router
