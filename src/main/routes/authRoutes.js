f3d0bc0797948a2e5006c721bd161ccf6fd7103e
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-06-27 15:29:12 +0500

    feat: logout

--- src/main/routes/authRoutes.js
+++ src/main/routes/authRoutes.js
@@ -1,8 +1,16 @@
-import { Router } from "express"
+import express from "express"
 import AuthService from "../services/authService.js";
 
-const router = Router()
+const router = express.Router()
 
 router.post("/login", AuthService.login)
+router.post('/logout', (req, res) => {
+    res.clearCookie('token', {
+        httpOnly: false,
+        secure: false,
+        sameSite: 'lax',
+        path: '/',
+    }).redirect(process.env.FRONTEND_URL + "/login");
+});
 
 export default router
