d8c8cf3ed1bfd9d353f66e0429911399aca94380
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-06-30 12:18:01 +0500

    fix: some fixes when delete and update models

--- src/main/dto/User.dto.js
+++ src/main/dto/User.dto.js
@@ -4,7 +4,7 @@ export const UserDto = z.object({
     email: z.string().email("Invalid email format"),
     full_name: z.string().nonempty("Full name is required"),
     role: z.enum(["client", "department-head", "executor"]),
-    specialty: z.string().nonempty("Specialty is required"),
+    specialty: z.string().nonempty("Specialty is required").optional(),
     office_id: z.number().gt(0).min(1, "Office ID is required")
 });
 
